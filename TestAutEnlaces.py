88da2f496e2b3fecbe9d3f7af8e47cfd79f335a2
dfloresva <64159332+dfloresva@users.noreply.github.com>
2024-09-27 15:25:11 -0500

    try to automatic send of msg coord.
    intento de automatizacion de envio de mensajes coordaindores

--- TestAutEnlaces.py
+++ TestAutEnlaces.py
@@ -1,4 +1,4 @@
-import pandas as pd
+""" import pandas as pd
 import webbrowser
 
 
@@ -57,3 +57,52 @@ menuSeleccion = MenuSeleccion(archivo_excel1)
 
 # salonTemp = "01"
 # print("SP_" + salonTemp)
+ """
+ 
+import pandas as pd
+import webbrowser
+
+class EnlaceExcel:
+    def __init__(self, archivo_excel, nombre_columna):
+        self.archivo_excel = archivo_excel
+        self.nombre_columna = nombre_columna
+        self.enlaces = []
+
+        self.leer_excel()
+        self.abrir_enlaces()
+
+    def leer_excel(self):
+        try:
+            df = pd.read_excel(self.archivo_excel, sheet_name=0)
+            if self.nombre_columna in df.columns:
+                self.enlaces = df[self.nombre_columna].dropna().tolist()
+                # print(self.enlaces)
+            else:
+                print(f"La columna '{self.nombre_columna}' no se encuentra en el archivo Excel.")
+        except Exception as e:
+            print(f"Error de lectura del archivo: {e}")
+
+    def abrir_enlaces(self):
+        for enlace in self.enlaces:
+            webbrowser.open(enlace)
+
+class MenuSeleccion:
+    def __init__(self, archivo_excel):
+        self.archivo_excel = archivo_excel
+        self.ingresar_salon(self.archivo_excel)
+
+    def ingresar_salon(self, archivo_excel):
+        while True:
+            salonTemp = input("Num salon (ingrese '0' para salir): ")
+            if salonTemp == "0":
+                print("Saliendo del programa.")
+                break
+
+            EnlaceExcel(archivo_excel, self.conc_String(salonTemp))
+
+    def conc_String(self, salon):
+        return "SP_" + salon
+
+# ASIGNACION DE VARIABLES DE DIRECCION Y ELECCION
+archivo_excel1 = "enlaces.xlsx"
+menuSeleccion = MenuSeleccion(archivo_excel1)
